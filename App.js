7eecf855d591bda4bac4fdf4c1d029d3b46a307e
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-08-12 09:48:02 -0300

    feat: create form

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from "react-native";
 import Title from "./src/components/Title";
+import Main from "./src/components/Main";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
+      <Main />
     </View>
   );
 }
