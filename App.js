1db7ca49935348a70785ce6de917117208310d8e
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-08-29 14:29:37 -0300

    feat: create QuoationList e HistoryGraphic

--- App.js
+++ App.js
@@ -7,12 +7,16 @@ import {
   StatusBar,
 } from "react-native";
 import CurrentPrice from "./src/components/CurrentPrice";
+import HistoryGraphic from "./src/components/HistoryGraphic";
+import QuotationList from "./src/components/QuotationList";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#f50d41" barStyle="light-content" />
       <CurrentPrice />
+      <HistoryGraphic />
+      <QuotationList />
     </SafeAreaView>
   );
 }
